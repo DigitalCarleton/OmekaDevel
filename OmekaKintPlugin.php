01ae732587ea7bf98ff9b214d9605a40cd71e047
Martin Bernard <nawm@carleton.edu>
2022-04-28 01:24:29 -0500

    Registering admin_hook

--- OmekaKintPlugin.php
+++ OmekaKintPlugin.php
@@ -3,11 +3,18 @@
 
 class OmekaKintPlugin extends Omeka_Plugin_AbstractPlugin
 {
-    protected $_hooks = array('initialize');
+    protected $_hooks = array('initialize', 'admin_head');
 
     function hookInitialize()
     {
         require 'kint.phar';
-        Kint\Renderer\RichRenderer::$theme = 'solarized.css';
+        Kint\Renderer\RichRenderer::$theme = 'solarized.css'; 
+        //Kint\Renderer\RichRenderer::$theme = '/mnt/c/MAMP/htdocs/omeka-3.0.1/plugins/OmekaKint/solarized.css';
     }
-}
+
+    public function hookAdminHead(){
+        (new Omeka_Controller_Action_Helper_FlashMessenger)->addMessage("test");
+    }
+
+
+}   
